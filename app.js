2ff17d3b957ad461d9e79227dfdeec2376d963c8
camshaft <bytheway.cameron@gmail.com>
2015-01-08 02:10:32 -0700

    add param checking

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ var service = require('poe-service-node');
 var app = module.exports = service();
 
 app.register('math', 'add', function(req, res) {
+  if (!Array.isArray(req.params)) return res.send('Invalid arguments');
+
   var answer = req.params.reduce(function(acc, num) {
     return acc + num;
   }, 0);
+
   res.send(answer);
 });
